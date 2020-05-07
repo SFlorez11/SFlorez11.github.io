c3c53f2bc904add72db671e8df62c416f21238c5
Florez <62672902+SFlorez11@users.noreply.github.com>
2020-05-07 19:25:58 +0300

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function(){ 
+		var local_score  = 0,
+		global_score = 0;
+
+		for(let i = 0; i < 20; i++){
+			$(`.tab:eq(${i})`).attr('data-action', 'confirm');
+		}
+
+		$(".answer:nth-child(1) > input[name='test_radio_tab']").attr('data-value', '0');
+		$(".answer:nth-child(2) > input[name='test_radio_tab']").attr('data-value', '1');
+		$(".answer:nth-child(3) > input[name='test_radio_tab']").attr('data-value', '2');
+
+
+		$("input[name='test_radio_tab']").click(function(){
+			local_score = +$(this).attr('data-value');
+		});
+		$('.tab[data-action="confirm"]').click(function(){
+			global_score += local_score;
+			local_score = 0;
+		});
+		$('#get_res').click(function(){
+			$( ".results_text").html(global_score);
+	
+		});
+
+}
+);
